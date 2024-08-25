219cf7cab0ec178521aada7786880e2a1bfac0e4
Sandy <sandysjingaa@gmail.com>
2024-08-25 16:20:47 +0800

    update

--- src/tt_um_shuangyu_top.v
+++ src/tt_um_shuangyu_top.v
@@ -9,9 +9,7 @@
 module tt_um_shuangyu_top (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    /* verilator lint_off UNUSEDSIGNAL */
     input  wire [7:0] uio_in,   // IOs: Input path
-    /* verilator lint_on UNUSEDSIGNAL */
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
@@ -22,7 +20,7 @@ module tt_um_shuangyu_top (
     assign uio_oe[7:0] = 8'b1111_1111;  //bir all for output
 
     // List all unused inputs to prevent warnings
-    wire _unused = &{ena, ui_in[7:4], 1'b0};
+    wire _unused = &{ena, ui_in[7:4], uio_in[7:0], 1'b0};
 
     // All output pins must be assigned. If not used, assign to 0.
     assign uo_out[7:0] = SevenSegment[7:0];
